9c783a3b250cec1ecba2e714c61e35c4335f9551
Marcelo Bastos <mbastos@microsoft.com>
2017-08-15 15:08:20 -0700

    Added finalizer to the environamen namespance

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,19 @@
 e = new.env()
 
 reg.finalizer(e, function(e) {
-  message('Bye!')
+  message('Object Bye!')
 }, onexit = TRUE)
+
+
+finalize <- function(env) {
+
+   print(ls(env))
+   message("Bye from Name space Finalizer")
+}
+
+
+.onLoad <- function(libname, pkgname) {
+   parent <- parent.env(environment())
+   print(str(parent))
+   reg.finalizer(parent, finalize, onexit= TRUE)
+}
